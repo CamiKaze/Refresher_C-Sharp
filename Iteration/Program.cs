10998403cf7c37a549ab65bd4279f49d1bd17ab2
CamiKaze <cameronsmith63@gmail.com>
2020-03-13 14:55:31 +0200

    Added iteration examples

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -4,9 +4,75 @@ namespace Iteration
 {
     class Program
     {
+        private static void IncrementForLoop()
+        {
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+        private static void DecrementForLoop()
+        {
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+        private static void IterateString()
+        {
+            var name = "Cameron Smith";
+
+            foreach (var character in name)
+            {
+                System.Console.WriteLine(character);
+            }
+        }
+        private static void IterateArray()
+        {
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                System.Console.WriteLine(number);
+            }
+        }
+        private static void WhileConditionNotMet()
+        {
+            while (true)
+            {
+                System.Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    System.Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+                break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Increment Loop");
+            IncrementForLoop();
+
+            System.Console.WriteLine("Decrement Loop");
+            DecrementForLoop();
+
+            System.Console.WriteLine("Loop over characters in string");
+            IterateString();
+
+            System.Console.WriteLine("Loop over index in array");
+            IterateArray();
+
+            System.Console.WriteLine("While a condition has not been met");
+            WhileConditionNotMet();
         }
     }
 }
