b30cd3cbe3f9e6f9987a2a403a3d05e0683b3960
CamiKaze <cameronsmith63@gmail.com>
2020-03-13 16:11:41 +0200

    Added ControlFlow Exercise

---
+++ ControlFlow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+
+            var sum = 0;
+            while (true)
+            {
+
+                System.Console.WriteLine("Please enter a number.");
+                var check = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(check))
+                {
+                    var temp = Convert.ToInt32(check);
+                    sum += temp;
+                    System.Console.WriteLine("Sum: " + sum);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
