676b54e6b218b36235a56c08d6d25e20d49c4d29
CamiKaze <cameronsmith63@gmail.com>
2020-03-18 12:08:56 +0200

    Added Array project

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,47 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 3, 6, 3, 45, 6, 7 };
+
+            // Length
+            System.Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 6);
+            System.Console.WriteLine("Index of 3: " + index);
+
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+            System.Console.WriteLine("Clear method");
+            foreach (var n in numbers)
+            {
+                System.Console.WriteLine(n);
+            }
+
+            // Copy()
+            int[] newArray = new int[3];
+            Array.Copy(numbers, newArray, 3);
+            System.Console.WriteLine("Copy method");
+            foreach (var n in newArray)
+            {
+                System.Console.WriteLine(n);
+            }
+
+            // Sort()
+            Array.Sort(numbers);
+            System.Console.WriteLine("Sort method");
+            foreach(var n in numbers)
+            {
+                System.Console.WriteLine(n);
+            }
+
+            // Reverse Method
+            Array.Reverse(numbers);
+            System.Console.WriteLine("Reverse method");
+            foreach(var n in numbers)
+            {
+                System.Console.WriteLine(n);
+            }
         }
     }
 }
