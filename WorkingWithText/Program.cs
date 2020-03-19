6a19afc3ac75c4b978acc4b68f751e2b63816215
IQB_Cameron <cameronsmith63@gmail.com>
2020-03-19 10:40:07 +0200

    Added various examples of working with text

---
+++ WorkingWithText/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Cameron Smith ";
+            System.Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            System.Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            // Not a good approach
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            System.Console.WriteLine("FirstName: " + firstName);
+            System.Console.WriteLine("LastName: " + lastName);
+
+            // Better approach
+            var names = fullName.Split(' ');
+            System.Console.WriteLine("FirstName: " + names[0]);
+            System.Console.WriteLine("LastName: " + names[1]);
+
+            // Replace Method
+            System.Console.WriteLine(fullName.Replace("Cameron", "Cammy"));
+
+            // Validation
+            if (String.IsNullOrWhiteSpace(" "))
+                System.Console.WriteLine("Invalid");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            System.Console.WriteLine(age);
+
+            float price = 29.95f;
+            System.Console.WriteLine(price.ToString("C0"));
+        }
+    }
+}
