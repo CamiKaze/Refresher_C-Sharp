2ee72df57d3c0af5572e873deeaacf553da4f953
IQB_Cameron <cameronsmith63@gmail.com>
2020-03-19 12:58:08 +0200

    Add StringBuilder basics

---
+++ StringBuilderBasics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+                
+            System.Console.WriteLine(builder);
+            System.Console.WriteLine("First Char: " + builder[0]);
+        }
+    }
+}
